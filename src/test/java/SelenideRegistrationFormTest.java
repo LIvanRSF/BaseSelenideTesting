5cb0a2676b72de02b7687233c516a32757604f15
Ivan L <fallouttoday@mail.ru>
2022-06-07 21:36:39 +0300

    Selenid registration form test 2nd edition

--- src/test/java/SelenideRegistrationFormTest.java
+++ src/test/java/SelenideRegistrationFormTest.java
@@ -14,46 +14,65 @@ public class SelenideRegistrationFormTest extends SelenideBaseClass {
     @DisplayName("Заполнение регистрационной формы")
     @Test
     public void successRegistration() {
+
+        //блок с данными вынесен отдельно, чтобы было по минимуму хардкода
         String firstName = "John";
         String lastName = "Doe";
         String email = "qaqa@yahoo.com";
         String gender = "Male";
         String phoneNumber = "9932223344";
+        String monthOfBirth = "August";
+        String yearOfBirth = "2001";
+        String dayOfBirth = "07";
         String subject = "Arts";
         String hobbies = "Sports";
-        String picture = "test.png";
+        String pictureName = "test.png";
+        String filePath = "src/test/resources/";
         String address = "Lenina 17";
         String state = "Haryana";
         String city = "Karnal";
 
+        //открываем сайт и убираем лишнее (рекламные баннеры)
         open("/automation-practice-form");
         executeJavaScript("$('footer').remove()");
         executeJavaScript("$('#fixedban').remove()");
 
+        //непосредственно работа с элементами сайта
         $("#firstName").setValue(firstName);
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(email);
         $("#genterWrapper").$(byText(gender)).click();
         $("#userNumber").setValue(phoneNumber);
+
+        //блок с календарем
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption(monthOfBirth);
+        $(".react-datepicker__year-select").selectOption(yearOfBirth);
+        $(".react-datepicker__day--0" + dayOfBirth).click();
+
         $("#subjectsInput").setValue(subject).pressEnter();
         $("#hobbiesWrapper").$(byText(hobbies)).click();
         $("#uploadPicture").uploadFile
-                               (new File("src/test/resources/" + picture));
+                               (new File(filePath + pictureName));
         $("#currentAddress").setValue(address);
         $("#state").scrollTo().click();
         $("#stateCity-wrapper").$(byText(state)).click();
         $("#city").click();
         $("#stateCity-wrapper").$(byText(city)).click();
         $("#submit").click();
+        System.out.println(" ");
 
+        // Проверка формы подтверждения регистрации. Данные на форме должны соответстовавать
+        // введенным ранее данным
         $(".modal-body").shouldHave(text(firstName + " " + lastName),
             text(email),
             text(gender),
             text(phoneNumber),
+            text(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth),
             text(gender),
             text(subject),
             text(hobbies),
-            text(picture),
+            text(pictureName),
             text(gender),
             text(address),
             text(state + " " + city));
