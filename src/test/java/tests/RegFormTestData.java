5569d866289a98c9f2610abf4a97310b797dd4bb
Ivan L <fallouttoday@mail.ru>
2022-06-08 00:45:13 +0300

    Selenid registration form "Page-object" pattern 3nd (with testData)

---
+++ src/test/java/tests/RegFormTestData.java
@@ -0,0 +1,21 @@
+package tests;
+
+public class RegFormTestData {
+
+    //блок с данными вынесен отдельно, чтобы было по минимуму хардкода
+    String firstName = "John";
+    String lastName = "Doe";
+    String userEmail = "qaqa@yahoo.com";
+    String gender = "Male";
+    String phoneNumber = "9932223344";
+    String monthOfBirth = "August";
+    String yearOfBirth = "2001";
+    String dayOfBirth = "07";
+    String subject = "Arts";
+    String hobby = "Sports";
+    String pictureName = "test.png";
+    String filePath = "src/test/resources/";
+    String address = "Lenina 17";
+    String state = "Haryana";
+    String city = "Karnal";
+}
--- src/test/java/tests/SelenideRegistrationFormTest.java
+++ src/test/java/tests/SelenideRegistrationFormTest.java
@@ -11,49 +11,36 @@ public class SelenideRegistrationFormTest extends SelenideBaseClass {
     @Test
     public void successRegistration() {
 
-        //блок с данными вынесен отдельно, чтобы было по минимуму хардкода
-        String firstName = "John";
-        String lastName = "Doe";
-        String userEmail = "qaqa@yahoo.com";
-        String gender = "Male";
-        String phoneNumber = "9932223344";
-        String monthOfBirth = "August";
-        String yearOfBirth = "2001";
-        String dayOfBirth = "07";
-        String subject = "Arts";
-        String hobby = "Sports";
-        String pictureName = "test.png";
-        String filePath = "src/test/resources/";
-        String address = "Lenina 17";
-        String state = "Haryana";
-        String city = "Karnal";
+        //Создаем класс с тестовыми данными
+        RegFormTestData testData = new RegFormTestData();
 
-        //непосредственно работа с элементами сайта
+        //Непосредственно работа с элементами сайта - заполнение формы регистрации, нажатие кнопки "Submit"
         new RegistrationFormPage().openPage()
-                                  .setFirstName(firstName)
-                                  .setLastName(lastName)
-                                  .setUserEmail(userEmail)
-                                  .setGender(gender)
-                                  .setPhoneNumber(phoneNumber)
-                                  .setDateOfBirth(monthOfBirth, yearOfBirth, dayOfBirth)
-                                  .setSubject(subject)
-                                  .setHobby(hobby)
-                                  .uploadUserFile(filePath, pictureName)
-                                  .setAddress(address)
-                                  .setStateAndCity(state, city)
+                                  .setFirstName(testData.firstName)
+                                  .setLastName(testData.lastName)
+                                  .setUserEmail(testData.userEmail)
+                                  .setGender(testData.gender)
+                                  .setPhoneNumber(testData.phoneNumber)
+                                  .setDateOfBirth(testData.monthOfBirth, testData.yearOfBirth, testData.dayOfBirth)
+                                  .setSubject(testData.subject)
+                                  .setHobby(testData.hobby)
+                                  .uploadUserFile(testData.filePath, testData.pictureName)
+                                  .setAddress(testData.address)
+                                  .setStateAndCity(testData.state, testData.city)
                                   .clickSubmitButton();
 
         // Проверка формы подтверждения регистрации. Данные на форме должны соответстовавать
         // введенным ранее данным
-        new RegistrationFormPage().checkResult("Student Name", firstName + " " + lastName)
-                                  .checkResult("Student Email", userEmail)
-                                  .checkResult("Gender", gender)
-                                  .checkResult("Mobile", phoneNumber)
-                                  .checkResult("Date of Birth", dayOfBirth + " " + monthOfBirth + "," + yearOfBirth)
-                                  .checkResult("Subjects", subject)
-                                  .checkResult("Hobbies", hobby)
-                                  .checkResult("Picture", pictureName)
-                                  .checkResult("Address", address)
-                                  .checkResult("State and City", state + " " + city);
+        new RegistrationFormPage().checkResult("Student Name", testData.firstName + " " + testData.lastName)
+                                  .checkResult("Student Email", testData.userEmail)
+                                  .checkResult("Gender", testData.gender)
+                                  .checkResult("Mobile", testData.phoneNumber)
+                                  .checkResult("Date of Birth",
+                                      testData.dayOfBirth + " " + testData.monthOfBirth + "," + testData.yearOfBirth)
+                                  .checkResult("Subjects", testData.subject)
+                                  .checkResult("Hobbies", testData.hobby)
+                                  .checkResult("Picture", testData.pictureName)
+                                  .checkResult("Address", testData.address)
+                                  .checkResult("State and City", testData.state + " " + testData.city);
     }
 }
